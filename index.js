fda82da1b850d5607f6aedd907a7b5d086491e77
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2014-11-30 08:20:59 -0500

    emails are being sent

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 "use strict";
 
 var randtoken = require('rand-token'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    nodemailer = require('nodemailer');
 
 var options = {
-    auth: {
-        user: 'user@gmail.com',
-        pass: 'password'
-    },
-    tempUserModel: null,
+    verificationURL: 'http://example.com/email-verification/',
 
+    //mongo-stuff
+    tempUserModel: null,
     // if a model hasn't been specified...
     tempUserCollection: 'temporary_users',
     hashPassword: false,
 
+    // emailing options
+    transportOptions: {
+        service: 'Gmail',
+        auth: {
+            user: 'user@gmail.com',
+            pass: 'password'
+        }
+    },
+    mailOptions: {
+        from: 'Do Not Reply <user@gmail.com>',
+        subject: 'Confirm your account',
+        html: '<p>Please confirm your account by clicking <a href="#{URL}">this link</a>. If you are unable to do so, copy and ' +
+                'paste the following link into your browser:</p><p>#{URL}</p>',
+        text: 'Please confirm your account by clicking the following link, or by copying and pasting it into your browser: #{URL}'
+    },
+    sendMailCallback: function(err, info) {
+        if (err) throw err;
+        else console.log(info.response);
+    }
 };
+
+var transporter = nodemailer.createTransport(options.transportOptions);
 
 var configure = function(o) {
     for (var key in o)
         options[key] = o[key];
+    transporter = nodemailer.createTransport(options.transportOptions);
 };
 
 var registerTempUser = function(user) {
@@ -61,6 +82,15 @@ var registerTempUser = function(user) {
     newTempUser.save(function(err) {
         if (err)
             throw err;
+
+        var email = newTempUser.email,
+            URL = options.verificationURL + newTempUser.GENERATED_VERIFYING_URL,
+            mailOptions = JSON.parse(JSON.stringify(options.mailOptions));
+        mailOptions.to = email;
+        mailOptions.html = mailOptions.html.replace(/#\{URL\}/g, URL);
+        mailOptions.text = mailOptions.text.replace(/#\{URL\}/g, URL);
+
+        transporter.sendMail(mailOptions, options.sendMailCallback);
     });
 };
 
