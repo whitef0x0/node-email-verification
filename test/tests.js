f5c8423b90c8118cda04d77d6f643ee7afbde59d
Frank Cash <cashc@acm.org>
2014-12-09 21:19:43 -0500

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -10,7 +10,7 @@ describe("main tests", function(){
   it("Tests the option object", function(){
     assert.typeOf(main.options.URLLength, 'number', "URL Length must be a number");
     assert.typeOf(main.options.expirationTime, 'number', "Expiration time must be a number");
-    assert.typeOf(main.options.verificationURL, 'string', "URL for verification must be a number");
+    assert.typeOf(main.options.verificationURL, 'string', "URL for verification must be a string");
   });
 
 
