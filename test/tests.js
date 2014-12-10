938bdc135d6eb4e90ebe14e2a809876ce3f12bb9
Frank Cash <cfcash@mtu.edu>
2014-12-09 20:35:46 -0500

    updates test

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,20 @@
 var assert = require('chai').assert;
+var should = require('chai').should();
+var expect = require('chai').expect;
 var main = require('../index');
 describe("main tests", function(){
+
+
+
+
   it("Tests the option object", function(){
     assert.typeOf(main.options.URLLength, 'number', "URL Length must be a number");
     assert.typeOf(main.options.expirationTime, 'number', "Expiration time must be a number");
     assert.typeOf(main.options.verificationURL, 'string', "URL for verification must be a number");
+  });
+
+
+  it("Tests sending email", function(done){
+    main.sendVerificationEmail("fizzbuzz@foo.bar","foo");
   });
 });
