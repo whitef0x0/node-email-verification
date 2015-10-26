d41bf72627e180c802bf8768d724fc4ccdd21067
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-10-26 19:33:11 -0400

    switched from bcrypt to bcryptjs and changed example to use 2 spaces; update version in package.json

--- examples/express/app/userModel.js
+++ examples/express/app/userModel.js
@@ -1,26 +1,26 @@
 var mongoose = require('mongoose'),
-    bcrypt = require('bcrypt');
+  bcrypt = require('bcryptjs');
 
 var userSchema = mongoose.Schema({
-    email: String,
-    pw: String,
-    salt: String,
+  email: String,
+  pw: String,
+  salt: String,
 });
 
 userSchema.pre('save', function(next) {
-    if (this.pw) {
-        this.pw = this.generateHash(this.pw);
-    }
-    next();
+  if (this.pw) {
+    this.pw = this.generateHash(this.pw);
+  }
+  next();
 });
 
 
 userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.pw);
+  return bcrypt.compareSync(password, this.pw);
 };
 
 module.exports = mongoose.model('real_users', userSchema);
